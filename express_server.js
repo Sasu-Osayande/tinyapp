760ea03e1c88c77780fc55b491c4ae607728a2f9
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-21 17:35:02 -0500

    Added a POST request to receive submissions from /urls/new. Installed the body-parser library and created a function to generate a random string

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
@@ -12,6 +15,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(length) {
+  length = 6;
+  let randomString = "";
+  let characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for (let i = 0; i <= length - 1; i ++) {
+    randomString += characters.charAt(Math.floor(Math.random() * (characters.length)));
+  }
+  return randomString;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,6 +43,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 // rendering urls_new

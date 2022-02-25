7a8a18a09dadd70ff0644b5ef6662dbae637dcf7
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-24 21:01:01 -0500

    Added the generateRandomString function to my helper file

--- helper.js
+++ helper.js
@@ -8,4 +8,17 @@ const getUsersByEmail = (email, database) => {
   return undefined;
 };
 
-module.exports = { getUsersByEmail };
+function generateRandomString(length) {
+  let randomString = "";
+  let characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for (let i = 0; i <= length - 1; i++) {
+    randomString += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return randomString;
+};
+
+module.exports = { getUsersByEmail, generateRandomString };
